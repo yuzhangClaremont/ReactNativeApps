fd1d90f8cfefd3d0a66228bc2434241679e6f40d
Yun <yuzhang@g.hmc.edu>
2019-09-28 16:33:12 -0700

    added title

--- rn-compelete-guide/App.js
+++ rn-compelete-guide/App.js
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 export default function App() {
   return (
 
-    <View >
+    <View style = { {padding:40} }>
       {/*  flexBoxDirection https://facebook.github.io/react-native/docs/flexbox */}
+      <Text style = { {textAlign: "center", color: "#823DB6", fontSize: 30} }> CheckList </Text>
       <View style = {  {padding: 30, flexDirection: "row", justifyContent: "center"}  }>
         <TextInput 
           placeholder = "add checklist" 
-          style = { {borderBottomWidth: 1, borderColor: "pink", width : 200} }/>
+          style = { {borderBottomWidth: 1, borderColor: "pink", width : "85%"} }/>
         <Button 
           
           title = "ADD"
